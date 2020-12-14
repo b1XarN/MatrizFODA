af1132708200016c4c4d070effea786ec8af8acd
Brian <brianss99@hotmail.com>
2020-12-14 09:05:33 -0500

    Boton de exportar a PDF hecho y Word en pruebas

--- empresa.php
+++ empresa.php
@@ -65,10 +65,11 @@
 
         <div class="row">
             <div class="col-12">
-                <table class="table">
+                <table class="table table-bordered" id="tablaFODA">
                     <tr>
                         <td>
                             <h2>MATRIZ FODA</h2>
+                            <h2 style="text-align:center; color:red;"><?=$empresa['nombreEmpresa']?></h2>
                         </td>
                         <td>
                             <h4>Oportunidades</h4>
@@ -289,8 +290,8 @@
 
         <div class="row justify-content-between">
             <div class="col-4">
-                <button class="btn btn-danger">Exportar a PDF</button>
-                <button class="btn btn-primary">Exportar a Word</button>
+                <button class="btn btn-danger" id="pdf">Exportar a PDF</button>
+                <button class="btn btn-primary" id="word">Exportar a Word</button>
             </div>
             <div class="col-4">
                 <a href="editar_matriz.php?id=<?=$_GET['id']?>" class="btn btn-success">Editar Matriz</a>
@@ -302,6 +303,7 @@
                 <a href="menu.php" class="btn btn-warning">Regresar</a>
             </div>
         </div>
+        
     </div>
 
 
@@ -311,5 +313,12 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    <!-- <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script> -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.22/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
+    <script type="text/javascript" src="js/tableExport.js"></script>
+    <script type="text/javascript" src="js/jquery.base64.js"></script>
+    <script type="text/javascript" src="js/exportarWord.js"></script>
+    <script type="text/javascript" src="js/exportarPDF.js"></script>
 </body>
 </html>
